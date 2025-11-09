98886c7f631aa592ef56dd9c8701f61fe473bd2a
avwaveaf <avwave12@gmail.com>
2025-11-09 19:50:23 +0700

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,18 @@
+// Main Objective of this project:
+// 1. Create text file on the disk
+// 2. Write data to that text file
+// 3. Close the stream buffer
+
+// New Concept
+// 1. Open Function
+// 2. Understanding Mode bits in Open Function
+
+#include <iostream>
+#include <fstream> // header for file handling read/write
+
+int main()
+{
+
+
+    return 0;
+}
